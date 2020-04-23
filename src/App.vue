170df4477d5e902b58f8768659f9ef18ebd009ae
DawidPiechota <dpiechotaa@gmail.com>
2020-04-23 21:48:47 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Graph msg="Graph testing"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Graph from './components/Graph.vue'
+import * as d3 from "d3";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Graph
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
