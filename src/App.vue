84657173ebc565cf2daa41c407af9d348a495dbd
DawidPiechota <dpiechotaa@gmail.com>
2020-04-24 02:09:38 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="app">
-    <VisualiseData msg="Graph testing"/>
+    <VisualiseData/>
   </div>
 </template>
 
 <script>
 
 import VisualiseData from './components/VisualiseData.vue'
-import * as d3 from "d3";
 
 export default {
   name: 'App',
--- src/components/VisualiseData.vue
+++ src/components/VisualiseData.vue
@@ -1,17 +1,47 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-  </div>
+      <ForceGraph :data="data"></ForceGraph>
+      <div style="padding-top: 10px; text-align: center" @click="changeData()">
+        <button>Change Data</button>
+      </div>
+    </div>
 </template>
 
-<script>
 
+<script>
+import ForceGraph from './ForceGraph.vue'
+import * as d3 from "d3";
 
 export default {
-    props: {
-    msg: String
-    },
     components: {
+      ForceGraph
+    },
+    data() {
+      return {
+        data: null,
+        dataList: ['data1.json', 'data2.json', 'data3.json'],
+      }
+    },
+    mounted() {
+      this.changeData();
+    },
+    methods: {
+      changeData() {
+    //     const dataIndex = Math.floor(Math.random() * this.dataList.length)
+    //     d3.json("https://gist.githubusercontent.com/DawidPiechota/f4e95b3d5d4539c453003136d8f25db9/raw/1f3df31a88a19e20464f9c6b9b12f6ca17e9201d/data2.json",
+    //     function(jsonData) {
+    //     this.data = data;
+    // });
+        //wrapper for fetch basically
+        d3.json("https://gist.githubusercontent.com/DawidPiechota/bc9eae88413e3546e7af2a92539f30bc/raw/8705fe2ba03868c55f6e436ddb453d8e834a74a6/data3.json")
+        .then(data => {this.data = data})
+
+        // d3.json(this.dataList[dataIndex]).then(data => {
+        //   this.data = data
+        // }).catch(error => {
+        //   console.error('Cannot proceed with simulation, failed to  retrieve data.')
+        // })
+      }
     }
-}
+  }
 </script>
